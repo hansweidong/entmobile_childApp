e730ec4642e9cf129fb9c791dc843efc881cbcaa
weidong_wu <wwdhao163@163.com>
2016-07-19 00:17:36 +0800

    code change

--- entmobile_ca/childApp_client/src/com/ca/mobile/ui/login/Activity_SelectedRole.java
+++ entmobile_ca/childApp_client/src/com/ca/mobile/ui/login/Activity_SelectedRole.java
@@ -1,7 +1,10 @@
 package com.ca.mobile.ui.login;
 
+import com.ca.mobile.BaseActivity;
+
 /**
  * Created by weidong_wu on 16/7/17.
  */
-public class Activity_SelectedRole {
+public class Activity_SelectedRole extends BaseActivity {
+
 }
